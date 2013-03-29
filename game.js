1ca0ae4e2be585c527e058d98447c05c21e2d081
tomo <cxytomo@gmail.com>
2013-03-29 16:03:23 +0800

    add something
    letterToGuess;try again button;exportImageData button;
    buttons not work yet.

--- game.js
+++ game.js
@@ -1,11 +1,28 @@
-(function canvasApp() {
+var Debugger = function() {};
+Debugger.log = function(message){
+	try {
+		console.log(message);
+	}
+	catch(exception) {
+		return;
+	}
+};
+
+window.addEventListener('load',windowloaded,false);
+
+function windowloaded() {
+	canvasApp();
+}
+
+function canvasApp() {
 	var canv = document.getElementById('game')
 	,	contxt = canv.getContext('2d')
 	,	guess = 3
-	,	message = "Guess The Letter From a(lower) to Z(higher)"
+	,	message = "Guess The Letter From a(lower) to z(lower)"
 	,	letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 	,	today = new Date()
 	,	letterToGuess = ""
+	,	letterToGArray = []
 	,	LowerOrHigher = ""
 	,	lettersGuessed
 	,	index
@@ -15,9 +32,11 @@
 	if(canvasSupport()) {
 		initGame();
 	}
+
 	function canvasSupport() {
 		return Modernizr.canvas;
 	}
+
 	function initGame(){
 		index = Math.floor(Math.random()*letters.length);
 		letterToGuess = letters[index];
@@ -25,6 +44,7 @@
 		window.addEventListener('keyup',keyPressed,true);
 		drawScreen();
 	}
+
 	function keyPressed(e){
 		if(!game && guess>0){
 			var keyPressed
@@ -40,7 +60,7 @@
 					game = true;
 					LowerOrHigher = "You win!";
 				}
-				if(guess >= 0) {
+				if(guess != 0) {
 					if(keyPressedIndex > index) {
 						LowerOrHigher = "Lower";
 					}
@@ -48,33 +68,47 @@
 						LowerOrHigher = "Higher";
 					}
 				} else {
+					letterToGArray.push(letterToGuess);
 					LowerOrHigher = "You loose!";
 				}
-				
+
 			} else{
+				letterToGArray.push(letterToGuess);
 				LowerOrHigher = "That is not a letter";
 			}
 		}
 		canv.height = "300";
 		drawScreen();
 	}
+	
 	function drawScreen(){
 		contxt.fillStyle = "#0081C2";
 		contxt.fillRect(0,0,500,300);
+		contxt.strokeStyle = "white";
+		contxt.strokeRect(5,5,490,290);
+		contxt.textBaseline = "top";
 		contxt.fillStyle = "white";
-		contxt.font = "10px _sans";
-		contxt.fillText(today,150,20);
+		contxt.font = "10px Arial";
+		contxt.fillText(today,160,18);
 		contxt.fillStyle = "white";
-		contxt.font = "18px _sans";
-		contxt.fillText(message,90,40);
+		contxt.font = "bold 22px Arial";
+		contxt.fillText(message,25,60);
+		contxt.fillStyle = "orange";
+		contxt.font = "18px Arial";
+		contxt.fillText("Guess: " + guess,220,90);
 		contxt.fillStyle = "white";
-		contxt.font = "18px _helvetic";
-		contxt.fillText("Guess:" + guess,220,60);
+		contxt.font = "18px Arial";
+		contxt.fillText("LowerOrHigher:  " + LowerOrHigher,25,200);
 		contxt.fillStyle = "white";
-		contxt.font = "18px _sans";
-		contxt.fillText("LowerOrHigher:" + LowerOrHigher,120,150);
+		contxt.font = "18px Arial";
+		contxt.fillText("lettersGuessed:  " + lettersGuessed.join(" "),25,230);
 		contxt.fillStyle = "white";
-		contxt.font = "18px _sans";
-		contxt.fillText("lettersGuessed:" + lettersGuessed.join(" "),20,280);
+		contxt.font = "18px Arial";
+		contxt.fillText("letterToGuess:  ",25,260);
+		if(letterToGArray[0]) {
+			contxt.fillStyle = "white";
+			contxt.font = "18px Arial";
+			contxt.fillText(letterToGArray[0],150,260);
+		}
 	}
-})();
+}
